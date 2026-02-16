491de85a9ade5a9062691693025215779688e8d4
lianet <jack.tolmak@yandex.ru>
2026-02-16 18:44:08 +0300

    add webp support

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -10,12 +10,7 @@ export const images = () => {
             }))
         )
         .pipe(app.plugins.newer(app.path.build.images))
-        // .pipe(
-        //     app.plugins.if(
-        //         app.isBuild,
-        //         webp()
-        //     )
-        // )
+        .pipe(webp())
         .pipe(
             app.plugins.if(
                 app.isBuild,
